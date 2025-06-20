2023518150ecda572f2806e1345eda620897e7a8
Shafayet Khan Shafee <sshafee@isrt.ac.bd>
2025-06-20 22:55:23 +0600

    feat: added customer and orders staging table

---
+++ jaffle_shop/models/staging/stg_customers.sql
@@ -0,0 +1,15 @@
+{{
+    config(
+          materialized = "table"
+        , tags = ["stg"]
+    )
+}}
+
+
+SELECT
+      id AS customer_id
+    , first_name
+    , last_name
+
+FROM 
+    {{ source("jaffle_shop", "customers") }}
---
+++ jaffle_shop/models/staging/stg_orders.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+          materialized = "table"
+        , tags = ["stg"]
+    )
+}}
+
+
+SELECT
+      id AS order_id
+    , user_id AS customer_id
+    , order_date
+    , status
+
+FROM 
+    {{ source("jaffle_shop", "orders") }}
